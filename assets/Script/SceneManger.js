218602577d05effa8979c94c296edc2b9832c659
zer01ike <wangkai_like@163.com>
2017-08-28 19:23:51 +0800

    edit the lanuch scene

--- assets/Script/SceneManger.js
+++ assets/Script/SceneManger.js
@@ -2,28 +2,22 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        label: {
-            default: null,
-            type: cc.Label
-        },
-        // defaults, set visually when attaching this script to the Canvas
-        text: '点击开始'
+        
     },
 
-    _changescene: function(event){
+    changescene: function(event){
         cc.director.loadScene("GameScene");
         console.log('MOUSE DOWN');
     },
 
     // use this for initialization
     onLoad: function () {
-        this.label.string = this.text;
         cc.view.enableAntiAlias(false);
-        this.label.node.on(cc.Node.EventType.MOUSE_DOWN,this._changescene,this);
-        this.label.node.on(cc.Node.EventType.TOUCH_START,this._changescene,this);
+        // this.label.node.on(cc.Node.EventType.MOUSE_DOWN,this._changescene,this);
+        // this.label.node.on(cc.Node.EventType.TOUCH_START,this._changescene,this);
     },
     // called every frame
-    update: function (dt) {
+    // update: function (dt) {
 
-    },
+    // },
 });
